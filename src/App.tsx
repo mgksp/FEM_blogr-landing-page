2d563d9494a882faff0e094d7ddd213d086625fc
Prabu <prabu.mgks@gmail.com>
2022-05-20 19:43:39 +0530

    footer mobile design done

--- src/App.tsx
+++ src/App.tsx
@@ -109,6 +109,57 @@ function App() {
           </div>
         </div>
       </main>
+
+      <footer className="relative font-ubuntu text-lg text-center py-20 bg-dkrBlackBlue text-grayishBlue rounded-tr-100 grid gap-12 place-content-center">
+        <img className="w-24 mb-6" src={logo} alt="" />
+        <div className="">
+          <h4 className="text-white font-bold mb-8">Products</h4>
+          <ul>
+            <li>Overview</li>
+            <li>Pricing</li>
+            <li>Marketplace</li>
+            <li>Features</li>
+            <li>Integrations</li>
+          </ul>
+        </div>
+        <div className="">
+          <h4 className="text-white font-bold mb-8">Company</h4>
+          <ul>
+            <li>About</li>
+            <li>Team</li>
+            <li>Blog</li>
+            <li>Careers</li>
+          </ul>
+        </div>
+        <div className="">
+          <h4 className="text-white font-bold mb-8">Connect</h4>
+          <ul>
+            <li>Contact</li>
+            <li>Newsletter</li>
+            <li>LinkedIn</li>
+          </ul>
+        </div>
+
+        <div className="absolute -bottom-7 py-1 text-xs font-medium text-dkrGrayishBlue w-full text-center">
+          Challenge by{" "}
+          <a
+            className="text-sm text-dkrBlue font-bold"
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            className="text-sm text-dkrBlue font-bold"
+            href="https://www.github.com/mgksp"
+            target="_blank"
+          >
+            Prabu
+          </a>
+          .
+        </div>
+      </footer>
     </>
   );
 }
